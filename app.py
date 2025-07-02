b5b91bf9bb9902f566f27d012cfb8974a7991d9d
S.Phurinat <6231501048@lamduan.mfu.ac.th>
2025-07-02 14:24:16 +0700

    not found

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,18 @@ def home():
 @app.route('/hello')
 def hello():
     return "Hello from Flask on Kubernetes!"
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
