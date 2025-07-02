8a5afb8a5ba13bece46029952089541b36765a67
S.Phurinat <6231501048@lamduan.mfu.ac.th>
2025-07-02 14:08:19 +0700

    .

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Home from Flask on Kubernetes!"
+
 @app.route('/hello')
 def hello():
     return "Hello from Flask on Kubernetes!"
